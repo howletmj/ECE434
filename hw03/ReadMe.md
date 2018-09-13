fe5240483edb4c5ad5e27d9c428e4af5bb66a86e
Demo User <debian@beaglebone.localdomain>
2018-09-13 18:40:13 +0000

    Finally works!

---
+++ hw03/ReadMe.md
@@ -0,0 +1,12 @@
+Hi Dr. Yoder,
+
+All i2c devices are on i2c bus 2. the rotary encoders are on P8_33, P8_35, P8_41, P8_42.
+1. I got the temp sensors to work and I wrote a shell script to interface with them and
+ report the temperature in degrees F.
+
+2. The dot matrix works with my etch a sketch program.
+
+3. The rotary encoders works with the etch a sketch program.
+ In addition. there is a  quit and clear button connected on pins P8_27,P8_28.
+
+ 
--- hw03/etch_matt_howlett.py
+++ hw03/etch_matt_howlett.py
@@ -2,6 +2,15 @@
 import Adafruit_BBIO.GPIO as GPIO
 import smbus
 import time
+from Adafruit_BBIO.Encoder import RotaryEncoder, eQEP2,eQEP1
+myEncoder = RotaryEncoder(eQEP1)
+myEncoder2 = RotaryEncoder(eQEP2)
+myEncoder.setAbsolute()
+myEncoder.enable()
+myEncoder.zero()
+myEncoder2.setAbsolute()
+myEncoder2.enable()
+myEncoder2.zero()
 bus = smbus.SMBus(2)
 matrix =0x70
 bus.write_byte_data(matrix, 0x21, 0)   # Start oscillator (p10)
@@ -9,52 +18,68 @@ bus.write_byte_data(matrix, 0x81, 0)   # Disp on, blink off (p11)
 bus.write_byte_data(matrix, 0xe7, 0)   # Full brightness (page 15)
 
 
-Buttons = ["P8_27","P8_28","P8_30","P8_32","P8_34","P8_36"]
-matrixarray = [0x00]*etchwinsize*etchwinsize*2
-
+Buttons = ["P8_27","P8_28"]
+matrixarray=[0x00]
 for myButton in Buttons:
 	print(myButton)
 	GPIO.setup(myButton, GPIO.IN)
 	GPIO.add_event_detect(myButton, GPIO.FALLING)
 def main():
 	etchwinsize=int(input("Window Size nxn n = ")) #window size plus one column for row numbers
+	global matrixarray
+	matrixarray = [0x00]*etchwinsize*2
 	cursorlocation = etchwinsize+2 #sets default location of cursor
 	display_array = ["1"]*etchwinsize*etchwinsize #initial display array
-	display_array = renderdisplay(cursorlocation,etchwinsize,display_array)#renders the text display
+	previouslocation =cursorlocation
+	display_array = renderdisplay(previouslocation,cursorlocation,etchwinsize,display_array)#renders the text display
 	while(1):
 		if GPIO.event_detected(Buttons[0]):
 			break
 		if GPIO.event_detected(Buttons[1]):
 			display_array = ["1"]*etchwinsize*etchwinsize
 			cursorlocation = etchwinsize+2
-			display_array = renderdisplay(cursorlocation,etchwinsize,display_array)#renders the text display
-		elif GPIO.event_detected(Buttons[2]):
-			if (cursorlocation/etchwinsize>2):
+			display_array = renderdisplay(previouslocation,cursorlocation,etchwinsize,display_array)#renders the text display
+		elif (myEncoder.position<0):#up
+			if (cursorlocation/etchwinsize>1):
+				previouslocation=cursorlocation
 				cursorlocation=cursorlocation-etchwinsize
-				display_array = renderdisplay(cursorlocation,etchwinsize,display_array)#renders the text display
-		elif GPIO.event_detected(Buttons[3]):
-			if (cursorlocation%etchwinsize>1):
+				display_array = renderdisplay(previouslocation,cursorlocation,etchwinsize,display_array)#renders the text display
+			myEncoder.zero()
+		elif (myEncoder2.position<0):#left
+			if (cursorlocation%etchwinsize>0):
+				previouslocation = cursorlocation
 				cursorlocation=cursorlocation-1
-				display_array = renderdisplay(cursorlocation,etchwinsize,display_array)#renders the text display
-		elif GPIO.event_detected(Buttons[4]):
+				display_array = renderdisplay(previouslocation,cursorlocation,etchwinsize,display_array)#renders the text display
+			myEncoder2.zero()
+		elif (myEncoder2.position>0):
 			if (cursorlocation%etchwinsize<etchwinsize-1):
+				previouslocation = cursorlocation
 				cursorlocation=cursorlocation+1
-				display_array = renderdisplay(cursorlocation,etchwinsize,display_array)#renders the text display
-		elif GPIO.event_detected(Buttons[5]):
-			if (cursorlocation/etchwinsize<etchwinsize-1):
+				display_array = renderdisplay(previouslocation,cursorlocation,etchwinsize,display_array)#renders the text display
+			myEncoder2.zero()
+		elif (myEncoder.position>0):
+			if (cursorlocation<etchwinsize*(etchwinsize-1)):
+				previouslocation=cursorlocation
 				cursorlocation=cursorlocation+etchwinsize
-				display_array = renderdisplay(cursorlocation,etchwinsize,display_array)#renders the text display
+				display_array = renderdisplay(previouslocation,cursorlocation,etchwinsize,display_array)#renders the text display
+			myEncoder.zero()
 
-def renderdisplay(cursorlocation,etchwinsize,display_array):
-	for index in range(0,etchwinsize*etchwinsize):#goes through the array and displays if row number, blank or x for where the cursor has been
-		if("x" not in str(display_array[index])):
-			display_array[index]=" "
+def renderdisplay(previouslocation,cursorlocation,etchwinsize,display_array):
 	display_array[cursorlocation] = "x"
-	for index in range(0,etchwinsize*etchwinsize):
-		if (index==0):
-			matrixarray[index] = display_array[index]
-		else:
-			matrixarray[index*2+1] = display_array[index]
+	global matrixarray
+	for index in range(etchwinsize*etchwinsize):
+                print(" " + str(display_array[index]) + " ", end="", flush=True)#prints without a newline
+                if(index%etchwinsize==etchwinsize-1):
+                        print("\n")
+
+	prevxloc = int(previouslocation/etchwinsize)
+	prevyloc = previouslocation%etchwinsize
+	xloc = int(cursorlocation/etchwinsize)
+	yloc = cursorlocation%etchwinsize
+	matrixarray[prevxloc*2+1]=0x00
+	matrixarray[prevxloc*2]=matrixarray[prevxloc*2]|(2**yloc)
+	matrixarray[xloc*2+1]=matrixarray[xloc*2+1]|(2**yloc)
+	print(cursorlocation)
 	bus.write_i2c_block_data(matrix,0,matrixarray)
 	return display_array# returns the array to keep track of the where the cursor has been
 main()
